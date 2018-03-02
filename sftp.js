a09cd8c7623b9187a4a37ef15025ebf05e524804
TobiWineKenobi <tobiwinekenobi@gmail.com>
2018-03-02 10:12:09 +0100

    Adding callback for successful upload
    After a successful upload there was no callback, so we added the callback to execute further funtions after a successful upload

--- sftp.js
+++ sftp.js
@@ -3,22 +3,31 @@ var Client = require('ssh2').Client;
 var fs = require('fs');
 
 function upload(credentials,pathToRemoteFile,pathToLocalFile,callback){
-  var pem = fs.readFileSync(credentials.privateKey);
-  var options = {
-      host: credentials.host,
-      port: credentials.port,
-      username: credentials.username,
-      privateKey: pem
-  };
-  var client = new Client();
-  client.on('ready', function() {
-    client.sftp(function(err, sftp) {
-      sftp.fastPut(pathToLocalFile, pathToRemoteFile ,function(err,res) {
-        if (err){ callback(err,null); }
-        client.end();
-      });
-    });
-  }).connect(options);
+
+ var pem = fs.readFileSync(credentials.privateKey);
+ var options = {
+     host: credentials.host,
+     port: credentials.port,
+     username: credentials.username,
+     privateKey: pem
+ };
+ var client = new Client();
+ client.on('ready', function() {
+   client.sftp(function(err, sftp) {
+     sftp.fastPut(pathToLocalFile, pathToRemoteFile ,function(err,res) {
+       if (err){
+                    client.end();
+                    callback(err,null);
+                }
+       // callback was only excecuted in an errorcase, so wie added the
+       // callback to an successful upload 
+                else {
+                    client.end();
+                    callback(null, null);
+                };
+            })
+   });
+ }).connect(options);
 }
 
 
